606e80855d4bed5a8061b6a0427636b754aafd30
fanxinye <397743448@qq.com>
2017-06-25 21:10:20 +0800

    添加

--- Git/src/com/itheima/test/Test01.java
+++ Git/src/com/itheima/test/Test01.java
@@ -4,4 +4,8 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("这是第一个git测试!");
 	}
+
+	public static void main(String[] args){
+		System.out.println("测试拉取!");
+	}
 }
